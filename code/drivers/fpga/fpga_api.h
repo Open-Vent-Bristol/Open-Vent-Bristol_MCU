aa2a89eb2467bbf9afd5f6323746e68ead8b15df
Donald Robson <donaldrobson@github>
2021-01-10 22:26:55 +0000

    Added event mask definitions. Tweaks to message structs. Heartbeat calculation now twos complement.

--- code/drivers/fpga/fpga_api.h
+++ code/drivers/fpga/fpga_api.h
@@ -14,10 +14,10 @@ typedef struct
   uint16_t measured_pressure;     // mmH2O
   uint16_t plateau_pressure;      // target pressure set by user, mmH2O
   uint16_t breath_rate;           // target breaths per minute
-  uint16_t exhale_inhale_ratio;   // target length of expiration x10 (unitless)
+  uint16_t inhale_period_cs;      // target length of expiration in centiseconds
   uint16_t percent_oxygen;
   uint16_t tidal_volume;          // ml
-  uint16_t padding_bytes;         // value only incidental in CRC calculation
+  uint16_t padding_bytes;         // reserved bytes (note: value affects CRC calculation result)
   uint16_t heartbeat;
   uint32_t crc32;
 } message_fpga_to_mcu_t;
@@ -35,42 +35,68 @@ typedef struct
 
 enum fpga_event_bits
 {
-  FPGA_EVENT_0 = (1u << 0u),
-  FPGA_EVENT_1 = (1u << 1u),
-  FPGA_EVENT_2 = (1u << 2u),
-  FPGA_EVENT_3 = (1u << 3u),
-  FPGA_EVENT_4 = (1u << 4u),
-  FPGA_EVENT_5 = (1u << 5u),
-  FPGA_EVENT_6 = (1u << 6u),
-  FPGA_EVENT_7 = (1u << 7u),
-  FPGA_EVENT_8 = (1u << 8u),
-  FPGA_EVENT_9 = (1u << 9u),
-  FPGA_EVENT_10 = (1u << 10u),
-  FPGA_EVENT_11 = (1u << 11u),
-  FPGA_EVENT_12 = (1u << 12u),
-  FPGA_EVENT_13 = (1u << 13u),
-  FPGA_EVENT_14 = (1u << 14u),
-  FPGA_EVENT_15 = (1u << 15u)
+  FPGA_EVENT_MODE_0                 = (1u << 0u),
+  FPGA_EVENT_MODE_1                 = (1u << 1u),
+  FPGA_EVENT_MODE_2                 = (1u << 2u),
+  FPGA_EVENT_MODE_MASK              = (FPGA_EVENT_MODE_2 | FPGA_EVENT_MODE_1 | FPGA_EVENT_MODE_0),
+  FPGA_EVENT_MOTOR_DISABLED         = (1u << 3u),    // The FPGA has disabled the motor
+  FPGA_EVENT_TIDAL_VOLUME_EXCEEDED  = (1u << 4u),    // The tidal volume limit is exceeded
+  FPGA_EVENT_ALARM_FAULT            = (1u << 5u),    // The FPGA requires the MCU to sound the alarm
+  // FPGA_EVENT_RESERVED_6             = (1u << 6u),
+  // FPGA_EVENT_RESERVED_7             = (1u << 7u),
+  // FPGA_EVENT_RESERVED_8             = (1u << 8u),
+  // FPGA_EVENT_RESERVED_9             = (1u << 9u),
+  // FPGA_EVENT_RESERVED_10            = (1u << 10u),
+  // FPGA_EVENT_RESERVED_11            = (1u << 11u),
+  // FPGA_EVENT_RESERVED_12            = (1u << 12u),
+  // FPGA_EVENT_RESERVED_13            = (1u << 13u),
+  // FPGA_EVENT_RESERVED_14            = (1u << 14u),
+  // FPGA_EVENT_RESERVED_15            = (1u << 15u)
+};
+
+/**
+ * Bits 0-2 of enum fpga_event_bits
+ */
+enum fpga_operating_mode
+{
+  FPGA_MODE_OFF                     = 0u,                                       // Power off request approved
+  FPGA_MODE_STANDBY                 = FPGA_EVENT_MODE_0,                        // Idle, power on
+  FPGA_MODE_PRESSURE_CONTROL        = FPGA_EVENT_MODE_1,                        // Use set points to control ventilation
+  FPGA_MODE_PRESSURE_SUPPORT        = (FPGA_EVENT_MODE_1 | FPGA_EVENT_MODE_0),  // Ventilation in response to patient demand
+  FPGA_MODE_STOP                    = FPGA_EVENT_MODE_2,                        // Used only during calibration
+  FPGA_MODE_RUN                     = (FPGA_EVENT_MODE_2 | FPGA_EVENT_MODE_0),  // Used only during calibration
 };
 
 enum mcu_event_bits
 {
-  MCU_EVENT_0 = (1u << 0u),
-  MCU_EVENT_1 = (1u << 1u),
-  MCU_EVENT_2 = (1u << 2u),
-  MCU_EVENT_3 = (1u << 3u),
-  MCU_EVENT_4 = (1u << 4u),
-  MCU_EVENT_5 = (1u << 5u),
-  MCU_EVENT_6 = (1u << 6u),
-  MCU_EVENT_7 = (1u << 7u),
-  MCU_EVENT_8 = (1u << 8u),
-  MCU_EVENT_9 = (1u << 9u),
-  MCU_EVENT_10 = (1u << 10u),
-  MCU_EVENT_11 = (1u << 11u),
-  MCU_EVENT_12 = (1u << 12u),
-  MCU_EVENT_13 = (1u << 13u),
-  MCU_EVENT_14 = (1u << 14u),
-  MCU_EVENT_15 = (1u << 15u)
+  MCU_EVENT_POWER_REQUEST_OFF       = (1u << 0u), // Request permission to remove power
+  MCU_EVENT_POWER_REQUEST_ON        = (1u << 1u), // Default - should be set except to request off
+  // MCU_EVENT_RESERVED_2              = (1u << 2u),
+  MCU_EVENT_RESET_MOTOR             = (1u << 3u), // Reenables the motor when disabled by the FPGA
+  MCU_EVENT_ACTUATOR_FAULT          = (1u << 4u), // The actuator is stuck
+  MCU_EVENT_BAG_FAULT               = (1u << 5u), // The bag is damaged
+  MCU_EVENT_BLOCKAGE_FAULT          = (1u << 6u), // There is a blockage in the airway
+  MCU_EVENT_LEAK_FAULT              = (1u << 7u), // There is a leak in the airway
+  MCU_EVENT_LIMIT_SWITCH_FAULT      = (1u << 8u), // The primary limit switch is not responding
+  // MCU_EVENT_RESERVED_9              = (1u << 9u),
+  // MCU_EVENT_RESERVED_10             = (1u << 10u),
+  // MCU_EVENT_RESERVED_11             = (1u << 11u),
+  // MCU_EVENT_RESERVED_12             = (1u << 12u),
+  MCU_EVENT_BATTERY_STATE_0         = (1u << 13u),
+  MCU_EVENT_BATTERY_STATE_1         = (1u << 14u),
+  MCU_EVENT_BATTERY_STATE_MASK      = (MCU_EVENT_BATTERY_STATE_1 | MCU_EVENT_BATTERY_STATE_0),
+  // MCU_EVENT_RESERVED_15             = (1u << 15u)
+};
+
+/**
+ * Bits 13-14 of enum mcu_event_bits
+ */
+enum battery_status
+{
+  BATTERY_STATUS_MAINS        = 0u,                                                       // The battery is charging (or charged)
+  BATTERY_STATUS_DISCHARGING  = MCU_EVENT_BATTERY_STATE_0,                                // The battery is discharging
+  BATTERY_STATUS_LOW          = MCU_EVENT_BATTERY_STATE_1,                                // The battery requires charging urgently
+  BATTERY_STATUS_CRITICAL     = (MCU_EVENT_BATTERY_STATE_1 | MCU_EVENT_BATTERY_STATE_0),  // Battery power will be imminently withdrawn
 };
 
 /**
@@ -83,8 +109,7 @@ enum mcu_event_bits
 inline void fpga_heartbeat_calculate(
   message_mcu_to_fpga_t* const message_to_format, uint16_t received_heartbeat)
 {
-  // TODO - implementation to be confirmed - probably bitwise NOT as below
-  message_to_format->heartbeat = ~received_heartbeat;
+  message_to_format->heartbeat = ~received_heartbeat + 1u;
 }
 
 /**
@@ -105,7 +130,8 @@ void fpga_display_format_pressure_bar(int16_t pressure_mmH2O, uint16_t peak_pres
  */
 bool fpga_display_has_changed(void);
 
-/** Format a message with a copy of the cached display (including custom characters)
+/**
+ * Format a message with a copy of the cached display (including custom characters)
  *
  * @param message_mcu_to_fpga_t* Pointer to the message
  */
