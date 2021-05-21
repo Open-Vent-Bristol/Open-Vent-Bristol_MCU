f37d1c28f70884acf421383b6493d91a6526b556
Donald Robson <donaldrobson@github>
2021-05-21 22:50:54 +0100

    Added main.  Added remaining modules to build.  Removed thermistor driver.

---
+++ code/application/system/system.c
@@ -0,0 +1,36 @@
+// Copyright (c) 2021 <OpenVent-Bristol, Donald Robson>
+
+#include "system.h"
+#include "actuator/actuator.h"
+#include "adc/adc.h"
+#include "alarm/alarm.h"
+#include "clock/clock.h"
+#include "cooling/fan.h"
+#include "crc/crc.h"
+#include "pinmux/pinmux.h"
+#include "spi/spi.h"
+#include "uart/uart.h"
+#include "ventilation/ventilation.h"
+#include "fpga/messages.h"
+#include "hardware_event/interrupt_handlers.h"
+
+void system_init(void)
+{
+  // Initialise HAL components (not watchdog!)
+  clock_init();
+  pinmux_init();
+  crc_init();
+  adc_init();
+  spi_init();
+  uart_init();
+
+  // Initialise driver components
+  actuator_init();
+  alarm_init();
+  fan_init();
+
+  // Initialise app components
+  ventilation_init();
+  message_init();
+  interrupt_init();
+}
